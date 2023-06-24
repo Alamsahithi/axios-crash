0dddce5146fe348862e4f285f692487e9d6afb2f
Alamsahithi <75735501+Alamsahithi@users.noreply.github.com>
2023-06-24 20:16:05 +0530

    Update start.js

--- start.js
+++ start.js
@@ -1,55 +1,145 @@
 // GET REQUEST
-function getTodos() {
-  console.log('GET Request');
+async function getTodos() {
+  try {
+    const response = await axios.get("/todos");
+    showOutput(response);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // POST REQUEST
-function addTodo() {
-  console.log('POST Request');
+async function addTodo() {
+  try {
+    const response = await axios.post("/todos", {
+      title: "New Todo",
+      completed: false
+    });
+    showOutput(response);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // PUT/PATCH REQUEST
-function updateTodo() {
-  console.log('PUT/PATCH Request');
+async function updateTodo() {
+  try {
+    const response = await axios.patch("/todos/1", {
+      title: "Updated Todo",
+      completed: true
+    });
+    showOutput(response);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // DELETE REQUEST
-function removeTodo() {
-  console.log('DELETE Request');
+async function removeTodo() {
+  try {
+    const response = await axios.delete("/todos/1");
+    showOutput(response);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // SIMULTANEOUS DATA
-function getData() {
-  console.log('Simultaneous Request');
+async function getData() {
+  try {
+    const [todos, posts] = await Promise.all([
+      axios.get("/todos"),
+      axios.get("/posts")
+    ]);
+    showOutput(todos);
+    showOutput(posts);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // CUSTOM HEADERS
-function customHeaders() {
-  console.log('Custom Headers');
+async function customHeaders() {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer token123"
+      }
+    };
+    const response = await axios.post("/todos", {
+      title: "New Todo",
+      completed: false
+    }, config);
+    showOutput(response);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // TRANSFORMING REQUESTS & RESPONSES
-function transformResponse() {
-  console.log('Transform Response');
+async function transformResponse() {
+  try {
+    const options = {
+      transformResponse: axios.defaults.transformResponse.concat(function (data) {
+        data.title = data.title.toUpperCase();
+        return data;
+      })
+    };
+    const response = await axios.get("/todos/1", options);
+    showOutput(response);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // ERROR HANDLING
-function errorHandling() {
-  console.log('Error Handling');
+async function errorHandling() {
+  try {
+    const response = await axios.get("/todos");
+    showOutput(response);
+  } catch (error) {
+    if (error.response) {
+      // Request made and server responded with a status code outside the range of 2xx
+      console.error(error.response.data);
+      console.error(error.response.status);
+      console.error(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error(error.message);
+    }
+  }
 }
 
 // CANCEL TOKEN
-function cancelToken() {
-  console.log('Cancel Token');
+async function cancelToken() {
+  const source = axios.CancelToken.source();
+
+  try {
+    const response = await axios.get("/todos", {
+      cancelToken: source.token
+    });
+    showOutput(response);
+  } catch (error) {
+    if (axios.isCancel(error)) {
+      console.log("Request canceled", error.message);
+    } else {
+      console.error(error);
+    }
+  }
+
+  // Cancel the request
+  setTimeout(function () {
+    source.cancel("Request canceled");
+  }, 100);
 }
-
-// INTERCEPTING REQUESTS & RESPONSES
-
-// AXIOS INSTANCES
 
 // Show output in browser
 function showOutput(res) {
-  document.getElementById('res').innerHTML = `
+  document.getElementById("res").innerHTML = `
   <div class="card card-body mb-4">
     <h5>Status: ${res.status}</h5>
   </div>
@@ -81,17 +171,14 @@ function showOutput(res) {
     </div>
   </div>
 `;
-}
 
 // Event listeners
-document.getElementById('get').addEventListener('click', getTodos);
-document.getElementById('post').addEventListener('click', addTodo);
-document.getElementById('update').addEventListener('click', updateTodo);
-document.getElementById('delete').addEventListener('click', removeTodo);
-document.getElementById('sim').addEventListener('click', getData);
-document.getElementById('headers').addEventListener('click', customHeaders);
-document
-  .getElementById('transform')
-  .addEventListener('click', transformResponse);
-document.getElementById('error').addEventListener('click', errorHandling);
-document.getElementById('cancel').addEventListener('click', cancelToken);
+document.getElementById("get").addEventListener("click", getTodos);
+document.getElementById("post").addEventListener("click", addTodo);
+document.getElementById("update").addEventListener("click", updateTodo);
+document.getElementById("delete").addEventListener("click", removeTodo);
+document.getElementById("sim").addEventListener("click", getData);
+document.getElementById("headers").addEventListener("click", customHeaders);
+document.getElementById("transform").addEventListener("click", transformResponse);
+document.getElementById("error").addEventListener("click", errorHandling);
+document.getElementById("cancel").addEventListener("click", cancelToken);
